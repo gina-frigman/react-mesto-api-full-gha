7315ee0d6374ae3f2590239bac0e6dc769dd6135
yella <jina-3ya.ru>
2023-10-06 22:51:10 +0300

    fix: errorhandler

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 module.exports.errorHandler = ((err, req, res, next) => {
+  console.dir(err);
   const { statusCode = 500, message } = err;
-  res.status(err.statusCode).send({
+  res.status(statusCode).send({
     message: statusCode === 500
       ? 'на сервере произошла ошибка'
       : message,
